ca2e07c1924e9f6232830630db73adbf99c6ea56
Logan Riffell <lkriffell@gmail.com>
2020-07-01 14:17:52 -0600

    Complete restaurant class and test

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,37 +1,70 @@
 class Restaurant
-  attr_reader :name, :opening_time, :dishes
-
+  attr_reader :opening_time, :name, :dishes
   def initialize(opening_time, name)
     @opening_time = opening_time
     @name = name
     @dishes = []
   end
 
+  def closing_time(hours)
+    @opening_time = opening_time.split(":")
+    @opening_time[0] = opening_time[0].to_i + hours
+    if opening_time[0] > 12
+      ampm = "PM"
+    else
+      ampm = "AM"
+    end
+    if opening_time[0] == 13
+      opening_time[0] = 1
+    elsif opening_time[0] == 14
+      opening_time[0] = 2
+    elsif opening_time[0] == 15
+      opening_time[0] = 3
+    elsif opening_time[0] == 16
+      opening_time[0] = 4
+    elsif opening_time[0] == 17
+      opening_time[0] = 5
+    elsif opening_time[0] == 18
+      opening_time[0] = 6
+    elsif opening_time[0] == 19
+      opening_time[0] = 7
+    elsif opening_time[0] == 20
+      opening_time[0] = 8
+    elsif opening_time[0] == 21
+      opening_time[0] = 9
+    elsif opening_time[0] == 22
+      opening_time[0] = 10
+    elsif opening_time[0] == 23
+      opening_time[0] = 11
+    elsif opening_time[0] == 24
+      opening_time[0] = 12
+    end
+    @opening_time[0] = opening_time[0].to_s
+    opening_time.insert(1,":")
+    opening_time.insert(3,"#{ampm}").join
+  end
+
   def add_dish(dish)
     @dishes << dish
   end
 
-  def closing_time(hours)
-    (@opening_time.to_i + hours).to_s + ':00'
-  end
-
-  def open_for_lunch?
-    @opening_time.to_i <= 11
+  def open_for_lunch
+    @opening_time = opening_time.split(":")
+    @opening_time[0] = opening_time[0].to_i
+    if @opening_time[0] < 12
+      true
+    else
+      false
+    end
   end
 
   def menu_dish_names
-    names_upcased = []
-    @dishes.each do |dish|
-      names_upcased << dish.upcase
+    @dishes.map do |dish|
+      dish = dish.upcase
     end
-    names_upcased
   end
 
   def announce_closing_time(hours)
-    time = closing_time(hours) + "AM"
-    if closing_time(hours).to_i >= 12
-      time = (closing_time(hours).to_i - 12).to_s + ':00PM'
-    end
-    "#{@name} will be closing at #{time}"
+    "#{@name} will be closing at #{closing_time(hours)}"
   end
 end
